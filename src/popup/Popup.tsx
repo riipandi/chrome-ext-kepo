24ee28e3c5eac6a92470c17e76c7d2b082678df9
Aris Ripandi <riipandi@gmail.com>
2022-10-26 05:27:55 +0700

    initial commit

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'preact/hooks'
+import './Popup.css'
+
+export const Popup = () => {
+  const [version, _setVersion] = useState('0.1.0')
+
+  return (
+    <main>
+      <h3>Options Page!</h3>
+
+      <h6>v{version}</h6>
+
+      <a href="https://github.com/riipandi/chrome-ext-kepo" target="_blank">
+        Made with 🫰 by Aris Ripandi
+      </a>
+    </main>
+  )
+}
+
+export default Popup
